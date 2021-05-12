c63abc2186a8553a20a1af806c92aa71362e7cc5
Julianna-szabo <szaberez@studen.ceu.edu>
2021-05-12 17:10:48 +0000

    New functions in get_usdhuf and playing around with more options.

--- R/forint.R
+++ R/forint.R
@@ -6,7 +6,8 @@
 #' @examples
 #' forint(42)
 #' forint(10000000.213214)
+#' forint(1:10)
 forint <- function(x) {
-  assert_number(x)
+  assert_numeric(x)
   dollar(x, prefix = "", suffix = " Ft")
 }
